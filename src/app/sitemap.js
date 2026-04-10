7df280e47a09ad84e45ce3ccf12478b335566e3c
Ngoc Nhan <60886911+petertran410@users.noreply.github.com>
2026-04-10 10:10:28 +0700

    Add sitemap

--- src/app/sitemap.js
+++ src/app/sitemap.js
@@ -9,38 +9,80 @@ export default function sitemap() {
     {
       url: domain,
       lastModified: new Date(),
-      changeFrequency: 'monthly',
+      changeFrequency: 'daily',
       priority: 1
-    },
-    {
-      url: `${domain}/gioi-thieu`,
-      lastModified: new Date(),
-      changeFrequency: 'monthly',
-      priority: 0.8
-    },
-    {
-      url: `${domain}/cong-thuc-pha-che`,
-      lastModified: new Date(),
-      changeFrequency: 'monthly',
-      priority: 0.8
-    },
-    {
-      url: `${domain}/tin-tuc`,
-      lastModified: new Date(),
-      changeFrequency: 'weekly',
-      priority: 0.8
     },
     {
       url: `${domain}/nguyen-lieu-pha-che`,
       lastModified: new Date(),
       changeFrequency: 'weekly',
+      priority: 0.9
+    },
+    {
+      url: `${domain}/nguyen-lieu-pha-che/mut-pha-che-lermao`,
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
       priority: 0.8
+    },
+    {
+      url: `${domain}/nguyen-lieu-pha-che/cac-loai-topping-tra-sua`,
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.8
+    },
+    {
+      url: `${domain}/nguyen-lieu-pha-che/cac-loai-topping-tra-sua/tran-chau-nau-nhanh`,
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.7
+    },
+    {
+      url: `${domain}/nguyen-lieu-pha-che/cac-loai-topping-tra-sua/thach-no-hat-no`,
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.7
+    },
+    {
+      url: `${domain}/nguyen-lieu-pha-che/bot-pha-che-lermao`,
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.7
+    },
+    {
+      url: `${domain}/nguyen-lieu-pha-che/siro-pha-che-lermao`,
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.7
+    },
+    {
+      url: `${domain}/cong-thuc-pha-che`,
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.6
+    },
+    {
+      url: `${domain}/workshop-pha-che`,
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.6
+    },
+    {
+      url: `${domain}/tin-tuc`,
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.6
+    },
+    {
+      url: `${domain}/gioi-thieu`,
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.6
     },
     {
       url: `${domain}/lien-he`,
       lastModified: new Date(),
-      changeFrequency: 'weekly',
-      priority: 0.8
+      changeFrequency: 'monthly',
+      priority: 0.6
     }
   ];
 }
